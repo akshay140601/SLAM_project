baa20e278aaab989b1b1c6768576e278bc5470d4
Sai-Yarlagadda <144404355+Sai-Yarlagadda@users.noreply.github.com>
2023-11-09 14:23:45 -0500

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,12 +1,13 @@
 import cv2
 import os
+import numpy as np
 
 class data_loading():
     def __init__(self) -> None:
         pass
-
         
     def frames(self, frame_number):
+        
         dataset_seq_00 = "dataset/sequences/00/"
         frame_number = '{:06d}'.format(frame_number)
         left_frame_path = os.path.join(dataset_seq_00, "image_0",
@@ -17,3 +18,14 @@ class data_loading():
         right_frame = cv2.imread(right_frame_path)
         left_frame = cv2.imread(left_frame_path)
         return right_frame, left_frame
+    
+    def ground_truth(self, frame_number, sequence):
+        sequence = '{:02d}'.format(sequence)
+        path = f"data_odometry_poses\\dataset\\poses\\00.txt"
+        file = open(path,'r')
+        ground_truth = []
+        for line in file:
+            line_nums = line.split()
+            ground_truth.append(line_nums)
+        return ground_truth[frame_number]
+        
