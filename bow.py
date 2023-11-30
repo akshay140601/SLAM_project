ff2f78cacb25575fadc01fcd2c54df6da034b697
Shubhra Aich <s.aich.72@gmail.com>
2023-11-29 21:43:02 -0500

    bow offset added

--- bow.py
+++ bow.py
@@ -81,7 +81,7 @@ class BoW(object) :
             
             file_list = sorted(os.listdir(dir_))
             # random.shuffle(file_list);
-            file_list = file_list[:50];
+            file_list = file_list[:1000];
             print("Extracting features ...");
             features = [];
             for fname in tqdm(file_list, desc=f"{i+1}") :
@@ -100,25 +100,28 @@ class BoW(object) :
 
             break;
 
-
     def add_frame(self, fpath) :
         self.features.append( self.get_image_features(fpath) );
 
     def is_loop_closure(self, offset, stride, thresh, closure_l) :
-        if stride > len(self.features) :
+        n = len(self.features);
+
+        if offset + stride > n :
             return False;
 
-        if len(self.features)-stride-1 < 0 :
+        i_end = n - offset - stride - 1;
+        if i_end < 0 :
             return False;
 
         x0 = self.features[-1];
         scores = [];
-        for i in range(len(self.features)-stride-1, -1, -stride) :
+        for i in range(i_end, -1, -stride) :
             # print(i);
             score = self.get_hist_similarity(x0, self.features[i]);
             scores.append((score, i));
 
         scores.sort(reverse=True);
+        from pprint import pprint; pprint(scores);
         top_score, top_i = scores[0];
         if top_score > thresh :
             closure_l.append(len(self.features)-1);
