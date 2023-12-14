a6c808fbc1f4d7a27cefc139ac0dc613d47c2909
Akshay Badagabettu <74955326+akshay140601@users.noreply.github.com>
2023-12-13 23:15:31 -0500

    Main final update

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 import yaml
 from dataloader import DataLoader
-from visual_odometry import visual_odometry
-#from utils import root_mean_squared_error
-import numpy as np
+from frontend import frontend
 
-
-# Driver Code
 if __name__ == '__main__':
 
-    # Load Config File
     with open("config/initial_config.yaml", "r") as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as error:
             print(error)
 
-    # Declare Necessary Variables
     sequence = config['data']['sequence']
 
-    # Create Instances
     data_handler = DataLoader(sequence=sequence)
-
-    # Reset frames to start from the beginning of the image list on a new run. Because we are using generators
     data_handler.reset_frames()
 
-    # Estimated trajectory by our algorithm pipeline
-    trajectory = visual_odometry(data_handler)
-
+    trajectory = frontend(data_handler)
