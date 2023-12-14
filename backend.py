622096cb69285d0d3eb02a606f176c05f15f7976
Akshay Badagabettu <74955326+akshay140601@users.noreply.github.com>
2023-12-13 23:18:42 -0500

    Backend final update

--- backend.py
+++ backend.py
@@ -161,7 +161,7 @@ def Powells_dog_leg_optimization(initial_poses, loop_closure_frames):
 
     return updated_poses
 
-def Pose3_ISAM2_example(initial_poses, loop_closure_frames):
+def ISAM2(initial_poses, loop_closure_frames):
     """Perform 3D SLAM given ground truth poses as well as simple
     loop closure detection."""
     lc_frames = loop_closure_frames.copy()
@@ -201,10 +201,10 @@ def Pose3_ISAM2_example(initial_poses, loop_closure_frames):
 
     # Create iSAM2 parameters which can adjust the threshold necessary to force relinearization and how many
     # update calls are required to perform the relinearization.
-    '''parameters = gtsam.ISAM2Params()
+    parameters = gtsam.ISAM2Params()
     parameters.setRelinearizeThreshold(0.01)
     parameters.relinearizeSkip = 1
-    isam = gtsam.ISAM2(parameters)'''
+    isam = gtsam.ISAM2(parameters)
 
     # Create the ground truth poses of the robot trajectory.
     true_poses = [gtsam.Pose3(pose) for pose in initial_poses]
@@ -259,12 +259,106 @@ def Pose3_ISAM2_example(initial_poses, loop_closure_frames):
             initial_estimate.insert(i + 2, noisy_estimate)
 
         # Perform incremental update to iSAM2's internal Bayes tree, optimizing only the affected variables.
+        isam.update(graph, initial_estimate)
+        current_estimate = isam.calculateEstimate()
+        initial_estimate.clear()
 
-    current_estimate = gtsam.LevenbergMarquardtOptimizer(graph, initial_estimate).optimize()
+    '''params = gtsam.DoglegParams()
+    params.setMaxIterations(150)
+    params.setRelativeErrorTol(1e-3)
+
+    optimizer = gtsam.gtsam.DoglegOptimizer(graph, initial_estimate, params)
+    current_estimate = optimizer.optimize()'''
     #current_estimate = isam.calculateEstimate()
     #print(current_estimate)
     for i in range(1, initial_poses.shape[0]+1):
         #current_estimate = isam.calculateEstimate()
+        ith_pose = current_estimate.atPose3(i).matrix()
+        updated_poses[i-1] = ith_pose[:3, :]
+
+    return updated_poses
+
+def LM(initial_poses, loop_closure_frames):
+    """Perform 3D SLAM given ground truth poses as well as simple
+    loop closure detection."""
+    lc_frames = loop_closure_frames.copy()
+    updated_poses = np.zeros((initial_poses.shape[0], initial_poses.shape[1], initial_poses.shape[2]))
+
+    prior_xyz_sigma = 1e-4
+    prior_rpy_sigma = 1e-6
+    odometry_xyz_sigma = 0.001
+    odometry_rpy_sigma = 0.0001
+    PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([prior_rpy_sigma*np.pi/180,
+                                                                prior_rpy_sigma*np.pi/180,
+                                                                prior_rpy_sigma*np.pi/180,
+                                                                prior_xyz_sigma,
+                                                                prior_xyz_sigma,
+                                                                prior_xyz_sigma]))
+    ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([odometry_rpy_sigma*np.pi/180,
+                                                                odometry_rpy_sigma*np.pi/180,
+                                                                odometry_rpy_sigma*np.pi/180,
+                                                                odometry_xyz_sigma,
+                                                                odometry_xyz_sigma,
+                                                                odometry_xyz_sigma]))
+
+    graph = gtsam.NonlinearFactorGraph()
+    initial_estimate = gtsam.Values()
+
+    true_poses = [gtsam.Pose3(pose) for pose in initial_poses]
+
+    odometry_tf = [true_poses[i-1].transformPoseTo(true_poses[i]) for i in range(1, len(true_poses))]
+    odometry_xyz = [(odometry_tf[i].x(), odometry_tf[i].y(), odometry_tf[i].z()) for i in range(len(odometry_tf))]
+    odometry_rpy = [odometry_tf[i].rotation().rpy() for i in range(len(odometry_tf))]
+
+    noisy_measurements = [np.random.multivariate_normal(np.hstack((odometry_rpy[i],odometry_xyz[i])), \
+                                                        ODOMETRY_NOISE.covariance()) for i in range(len(odometry_tf))]
+
+
+    graph.push_back(gtsam.PriorFactorPose3(1, true_poses[0], PRIOR_NOISE))
+    initial_estimate.insert(1, true_poses[0].compose(gtsam.Pose3(
+        gtsam.Rot3.Rodrigues(0, 0, 0), gtsam.Point3(0, 0, 0))))
+
+    current_estimate = initial_estimate
+    for i in range(len(odometry_tf)):
+
+        noisy_odometry = noisy_measurements[i]
+
+        noisy_tf = gtsam.Pose3(gtsam.Rot3.RzRyRx(noisy_odometry[:3]), noisy_odometry[3:6].reshape(-1,1))
+
+        #noisy_tf = gtsam.Pose3(gtsam.Rot3.RzRyRx(odometry_rpy[i]), odometry_xyz[i])
+
+        if len(lc_frames) != 0:
+            if i == lc_frames[0]:
+                graph.push_back(gtsam.BetweenFactorPose3(i + 1, lc_frames[1] + 1, noisy_tf, ODOMETRY_NOISE))
+                del lc_frames[0:3]
+                graph.push_back(gtsam.BetweenFactorPose3(i + 1, i + 2, noisy_tf, ODOMETRY_NOISE))
+                noisy_estimate = current_estimate.atPose3(i + 1).compose(noisy_tf)
+                initial_estimate.insert(i + 2, noisy_estimate)
+            else:
+                graph.push_back(gtsam.BetweenFactorPose3(i + 1, i + 2, noisy_tf, ODOMETRY_NOISE))
+                noisy_estimate = current_estimate.atPose3(i + 1).compose(noisy_tf)
+                initial_estimate.insert(i + 2, noisy_estimate)
+
+        else:
+            graph.push_back(gtsam.BetweenFactorPose3(i + 1, i + 2, noisy_tf, ODOMETRY_NOISE))
+            noisy_estimate = current_estimate.atPose3(i + 1).compose(noisy_tf)
+            initial_estimate.insert(i + 2, noisy_estimate)
+
+    '''parameters = gtsam.LevenbergMarquardtParams()
+    parameters.setRelativeErrorTol(1e-5)
+    parameters.setAbsoluteErrorTol(1e-5)
+
+    parameters.setlambdaInitial(1e-1)
+    parameters.setlambdaFactor(100.0)
+    #parameters.setlambdaFactorFailed(100.0)
+    parameters.setlambdaUpperBound(1e100)
+    parameters.setlambdaLowerBound(1e-100)
+    parameters.setMaxIterations(200)'''
+    parameters = gtsam.LevenbergMarquardtParams()
+    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial_estimate, parameters)
+    current_estimate = optimizer.optimize()
+
+    for i in range(1, initial_poses.shape[0]+1):
         ith_pose = current_estimate.atPose3(i).matrix()
         updated_poses[i-1] = ith_pose[:3, :]
 
